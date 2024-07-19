3c2b9f52011acc44eee86ec0b785794c3d82c9cf
Andre Bono <greatone0520@naver.com>
2024-07-19 09:20:55 +0900

    .

--- search.js
+++ search.js
@@ -1,26 +1,18 @@
-function searchBooks() {
-  const query = $("#query").val();
+async function searchBooks() {
+  console.log("검색한다");
+  const query = document.getElementById("query").value;
   const apiKey = "ttbwsnah05200918001";
-  const url = `https://www.aladin.co.kr/ttb/api/ItemSearch.aspx?ttbkey=${apiKey}
-    &Query=${encodeURIComponent(query)}&QueryType=Title&MaxResults=10
-    &start=1&SearchTarget=Book&output=JS&Version=20131101`;
+  const url = `http://www.aladin.co.kr/ttb/api/ItemSearch.aspx?ttbkey=${apiKey}
+  &Query=${encodeURIComponent(query)}&QueryType=Title&MaxResults=10
+  &start=1&SearchTarget=Book&output=JS&Version=20131101`;
 
-  $.ajax({
-    url: url,
-    type: "GET",
-    dataType: "jsonp", // JSONP를 사용할 경우, dataType을 jsonp로 설정해야 함
-    success: function (data) {
-      console.log("API 응답:", data); // API 응답을 콘솔에 출력하여 확인
-      if (data && data.item) {
-        displayResults(data.item);
-      } else {
-        console.error("API 응답에 item 필드가 없습니다.");
-      }
-    },
-    error: function (xhr, status, error) {
-      console.error("데이터를 가져오는 중 오류 발생:", error);
-    },
-  });
+  try {
+    const response = await fetch(url);
+    const data = await response.json();
+    displayResults(data.item);
+  } catch (error) {
+    console.error("Error fetching data:", error);
+  }
 }
 
 function displayResults(books) {
